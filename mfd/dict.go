50c569e405d0d2b869394ae930a02750bf9be1f7
Sergey Bykov <sergeyfast@gmail.com>
2020-03-19 11:50:48 +0300

    Add autotranslation for EN lang

--- mfd/dict.go
+++ mfd/dict.go
@@ -1,5 +1,12 @@
 package mfd
 
+import (
+	"strings"
+
+	"github.com/fatih/camelcase"
+	"github.com/jinzhu/inflection"
+)
+
 const (
 	EnLang = "en"
 	RuLang = "ru"
@@ -36,12 +43,64 @@ var presetsTranslations = map[string]map[string]string{
 
 		"actions": "Действия",
 	},
+	EnLang: {
+		"add":    "Add",
+		"edit":   "Edit",
+		"delete": "Delete",
+
+		"user":       "User",
+		"users":      "Users",
+		"category":   "Category",
+		"categories": "Categories",
+		"tag":        "Tag",
+		"tags":       "Tags",
+
+		"title":       "Title",
+		"description": "Description",
+		"foreword":    "Foreword",
+		"content":     "Content",
+		"email":       "Email",
+		"login":       "Login",
+		"image":       "Image",
+		"password":    "Password",
+		"alias":       "Alias",
+		"status":      "Status",
+		"statusId":    "Status",
+
+		"createdAt":  "Created at",
+		"modifiedAt": "Modified at",
+		"deletedAt":  "Deleted at",
+
+		"actions": "Actions",
+	},
 }
 
 func Translate(lang, key string) string {
+	// check current lang
 	if _, ok := presetsTranslations[lang]; !ok {
 		return ""
 	}
 
-	return presetsTranslations[lang][key]
+	// try to find from base translations
+	if found, ok := presetsTranslations[lang][key]; ok {
+		return found
+	}
+
+	// try to convert camelcase for EN
+	if lang == EnLang && key != "" {
+		w := key
+		if strings.HasSuffix(key, "Ids") {
+			w = strings.TrimSuffix(key, "Ids")
+			w = inflection.Plural(w)
+		} else {
+			w = strings.TrimSuffix(key, "Id")
+		}
+
+		splitted := camelcase.Split(w)
+		splitted[0] = strings.Title(splitted[0]) // uppercase first letter
+
+		return strings.Join(splitted, " ")
+	}
+
+	return ""
 }
