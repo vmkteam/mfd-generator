95e8dcbccad858dfc59597681c909bbef6a5a4ab
dmitryzavgorodniy <zavgorodniy@uteka.ru>
2023-03-14 18:01:11 +0300

    MFD-65 Fix and improve xml test

---
+++ docs/schema.sql
@@ -0,0 +1,81 @@
+﻿-- =============================================================================
+-- Diagram Name: test
+-- Created on: 14.03.2023 12:05:11
+-- Diagram Version: 
+-- =============================================================================
+
+CREATE TABLE "news" (
+	"newsId" SERIAL NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"preview" varchar(255),
+	"content" text,
+	"categoryId" int4 NOT NULL,
+	"tagIds" int4[],
+	"createdAt" timestamp with time zone NOT NULL DEFAULT NOW(),
+	"publishedAt" timestamp with time zone,
+	"statusId" int4 NOT NULL,
+	PRIMARY KEY("newsId")
+);
+
+COMMENT ON COLUMN "news"."newsId" IS 'id новости';
+
+COMMENT ON COLUMN "news"."title" IS 'Заголовок новости';
+
+COMMENT ON COLUMN "news"."preview" IS 'Ссылка на фото-превью новости';
+
+COMMENT ON COLUMN "news"."content" IS 'Контент новости';
+
+CREATE TABLE "statuses" (
+	"statusId" SERIAL NOT NULL,
+	PRIMARY KEY("statusId")
+);
+
+CREATE TABLE "categories" (
+	"categoryId" SERIAL NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"orderNumber" int4 NOT NULL,
+	"statusId" int4 NOT NULL,
+	PRIMARY KEY("categoryId")
+);
+
+CREATE TABLE "tags" (
+	"tagId" SERIAL NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"statusId" int4 NOT NULL,
+	PRIMARY KEY("tagId")
+);
+
+COMMENT ON COLUMN "tags"."tagId" IS 'id тега';
+
+COMMENT ON COLUMN "tags"."title" IS 'Текст тега';
+
+
+ALTER TABLE "news" ADD CONSTRAINT "Ref_news_to_statuses" FOREIGN KEY ("statusId")
+	REFERENCES "statuses"("statusId")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE "news" ADD CONSTRAINT "Ref_news_to_categories" FOREIGN KEY ("categoryId")
+	REFERENCES "categories"("categoryId")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE "categories" ADD CONSTRAINT "Ref_categories_to_statuses" FOREIGN KEY ("statusId")
+	REFERENCES "statuses"("statusId")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE "tags" ADD CONSTRAINT "Ref_tags_to_statuses" FOREIGN KEY ("statusId")
+	REFERENCES "statuses"("statusId")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+
--- generators/xml/generator_test.go
+++ generators/xml/generator_test.go
@@ -1,18 +1,62 @@
 package xml
 
 import (
+	"bufio"
+	"bytes"
+	"os"
+
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestGenerator_Generate(t *testing.T) {
-	generator := New()
+	Convey("TestGenerator_Generate", t, func() {
+		Convey("Check correct generate", func() {
+			generator := New()
 
-	generator.options.Def()
-	generator.options.URL = `postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable`
-	generator.options.Output = "./test/test.mfd"
+			generator.options.Def()
+			generator.options.URL = `postgres://postgres:postgres@localhost:5432/newsportal?sslmode=disable`
+			generator.options.Output = "result/newsportal.mfd"
+			generator.options.Packages = parseNamespacesFlag("common:users;vfs:vfsFiles,vfsFolders;portal:news,categories,tags")
 
-	if err := generator.Generate(); err != nil {
-		t.Errorf("generate error = %v", err)
-		return
+			err := generator.Generate()
+			So(err, ShouldBeNil)
+		})
+		Convey("Check generated files", func() {
+			files, err := os.ReadDir("testdata/result/")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for _, f := range files {
+				content, err := readFile("testdata/result/" + f.Name())
+				if err != nil {
+					t.Fatal(err)
+				}
+				necessaryContent, err := readFile("testdata/necessary/" + f.Name())
+				if err != nil {
+					t.Fatal(err)
+				}
+				So(content, ShouldEqual, necessaryContent)
+			}
+		})
+	})
+
+}
+
+func readFile(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
 	}
+	defer f.Close()
+
+	wr := bytes.Buffer{}
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		wr.WriteString(sc.Text())
+	}
+
+	return wr.String(), nil
 }
