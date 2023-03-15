07de9aa4b423349588bfebbf93d5c86bf0d81755
dmitryzavgorodniy <zavgorodniy@uteka.ru>
2023-03-15 13:00:02 +0300

    MFD-65 Fix dirs

--- generators/xml/generator_test.go
+++ generators/xml/generator_test.go
@@ -7,8 +7,8 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-// todo: fail if testdata/necessaryContent/actual/*.mfd,*.xml exists,
-// todo: panic if testdata/necessaryContent/actual/*.mfd exists and xml not exist
+// todo: fail if ../testdata/necessary-content/actual/*.mfd,*.xml exists,
+// todo: panic if ../testdata/necessary-content/actual/*.mfd exists and xml not exist
 func TestGenerator_Generate(t *testing.T) {
 	Convey("TestGenerator_Generate", t, func() {
 		Convey("Check correct generate", func() {
@@ -16,24 +16,24 @@ func TestGenerator_Generate(t *testing.T) {
 
 			generator.options.Def()
 			generator.options.URL = `postgres://postgres:postgres@localhost:5432/newsportal?sslmode=disable`
-			generator.options.Output = "testdata/necessaryContent/actual/newsportal.mfd"
-			generator.options.Packages = parseNamespacesFlag("common:users;vfs:vfsFiles,vfsFolders;portal:news,categories,tags")
+			generator.options.Output = "../testdata/necessary-content/actual/newsportal.mfd"
+			generator.options.Packages = parseNamespacesFlag("portal:news,categories,tags")
 
 			err := generator.Generate()
 			So(err, ShouldBeNil)
 		})
 		Convey("Check generated files", func() {
-			files, err := os.ReadDir("testdata/necessaryContent/actual/")
+			files, err := os.ReadDir("../testdata/necessary-content/actual/")
 			if err != nil {
 				t.Fatal(err)
 			}
 
 			for _, f := range files {
-				content, err := os.ReadFile("testdata/necessaryContent/actual/" + f.Name())
+				content, err := os.ReadFile("../testdata/necessary-content/actual/" + f.Name())
 				if err != nil {
 					t.Fatal(err)
 				}
-				expectedContent, err := os.ReadFile("testdata/necessaryContent/expected/" + f.Name())
+				expectedContent, err := os.ReadFile("../testdata/necessary-content/expected/" + f.Name())
 				if err != nil {
 					t.Fatal(err)
 				}
