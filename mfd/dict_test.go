00f8185844533024fe7ec7c428883720456a0f97
andrey <palchikov@uteka.ru>
2024-12-25 14:08:31 +0300

    Added test

--- mfd/dict_test.go
+++ mfd/dict_test.go
@@ -1,6 +1,9 @@
 package mfd
 
-import "testing"
+import (
+	"encoding/xml"
+	"testing"
+)
 
 func TestTranslate(t *testing.T) {
 	var testCases = []struct {
@@ -21,6 +24,58 @@ func TestTranslate(t *testing.T) {
 		s := Translate(tt.ln, tt.in)
 		if s != tt.out {
 			t.Errorf("%v: got %q, want %q", tt.ln, s, tt.out)
+		}
+	}
+}
+
+func TestAddedCustomTranslations(t *testing.T) {
+	var testCases = []struct {
+		name string
+		in   string
+		dict *Dict
+		want string
+	}{
+		{
+			"with updated word",
+			"user",
+			&Dict{
+				Entries: []Entry{
+					{XMLName: xml.Name{Local: "user"}, Value: "Пользователь (Обновленный)"},
+				},
+			},
+			"Пользователь (Обновленный)",
+		},
+		{
+			"with new word",
+			"newKey",
+			&Dict{
+				Entries: []Entry{
+					{XMLName: xml.Name{Local: "newKey"}, Value: "Изображение (Обновленное)"},
+				},
+			},
+			"Изображение (Обновленное)",
+		},
+		{
+			"with empty dict",
+			"",
+			&Dict{
+				Entries: []Entry{},
+			},
+			"",
+		},
+		{
+			"with nil dict",
+			"",
+			nil,
+			"",
+		},
+	}
+	for _, tt := range testCases {
+		old := Translate(RuLang, tt.in)
+		AddedCustomTranslations(tt.dict)
+		updated := Translate(RuLang, tt.in)
+		if updated != tt.want {
+			t.Errorf("%v: got %q, want %q", tt.in, old, tt.want)
 		}
 	}
 }
