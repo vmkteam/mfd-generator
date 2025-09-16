0ece29188a86c91c5d8d0c5cbee9460f86054ee8
andrey <palchikov@uteka.ru>
2025-09-16 16:23:44 +0300

    Add TableMapping in .mfd, tests and new flow with empty options.Packages

--- generators/xml/generator_test.go
+++ generators/xml/generator_test.go
@@ -1,11 +1,14 @@
 package xml
 
 import (
+	"encoding/xml"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	"github.com/vmkteam/mfd-generator/generators/testdata"
+	"github.com/vmkteam/mfd-generator/mfd"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -52,6 +55,68 @@ func TestGenerator_Generate(t *testing.T) {
 				}
 				So(string(content), ShouldResemble, string(expectedContent))
 			}
+		})
+	})
+}
+
+func helperLoadBytes(t *testing.T, path string) []byte {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return bytes
+}
+
+func TestGenerator_TableModel(t *testing.T) {
+	actualDir := t.TempDir()
+	mfdPathInActual := filepath.Join(actualDir, filepath.Base(testdata.PathExpectedMFD))
+
+	Convey("TestGenerator_TableModel", t, func() {
+		Convey("Check equal generate tableModels", func() {
+			var project, projectActual mfd.Project
+			err := xml.Unmarshal(helperLoadBytes(t, testdata.PathExpectedMFD), &project)
+			So(err, ShouldBeNil)
+			project.TableMapping = mfd.TableMapping{
+				Entries: []mfd.Entry{
+					{
+						XMLName: xml.Name{Local: "portal"},
+						Value:   "news,categories,tags",
+					},
+				},
+			}
+			err = mfd.SaveMFD(mfdPathInActual, &project)
+			So(err, ShouldBeNil)
+
+			err = xml.Unmarshal(helperLoadBytes(t, mfdPathInActual), &projectActual)
+			So(err, ShouldBeNil)
+			So(reflect.DeepEqual(projectActual.TableMapping, project.TableMapping), ShouldBeTrue)
+		})
+		Convey("Check equal Packages data", func() {
+			generator := New()
+
+			generator.options.Def()
+			generator.options.Packages = parseNamespacesFlag("common:users;vfs:vfsFiles,vfsFolders;news:news,categories,tags")
+
+			project := mfd.Project{TableMapping: mfd.TableMapping{
+				Entries: []mfd.Entry{
+					{
+						XMLName: xml.Name{Local: "common"},
+						Value:   "users",
+					},
+					{
+						XMLName: xml.Name{Local: "vfs"},
+						Value:   "vfsFiles,vfsFolders",
+					},
+					{
+						XMLName: xml.Name{Local: "news"},
+						Value:   "news,categories,tags",
+					},
+				},
+			}}
+
+			project.TableMapping.Packages()
+
+			So(reflect.DeepEqual(generator.options.Packages, project.TableMapping.Packages()), ShouldBeTrue)
 		})
 	})
 }
