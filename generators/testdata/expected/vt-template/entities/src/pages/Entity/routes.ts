4d529e3026afe9559139e4543653d27592388c81
andrey <palchikov@uteka.ru>
2026-03-25 17:14:15 +0300

    Add partial generate routes, fix tests, update vt template

--- generators/testdata/expected/vt-template/entities/src/pages/Entity/routes.ts
+++ generators/testdata/expected/vt-template/entities/src/pages/Entity/routes.ts
@@ -1,33 +1,36 @@
 /* eslint-disable */
 export default [
-  /* Category */
-  {
-    name: "categoryList",
-    path: "/categories",
-    component: () =>
-      import("@/pages/Entity/Category/List.vue"),
-    meta: {
-      breadcrumbs: ["dashboard", "categoryList"]
-    }
-  },
-  {
-    name: "categoryEdit",
-    path: "/categories/:id/edit",
-    component: () =>
-      import("@/pages/Entity/Category/Form.vue"),
-    meta: {
-      breadcrumbs: ["dashboard", "categoryList", "categoryEdit"]
-    }
-  },
-  {
-    name: "categoryAdd",
-    path: "/categories/add",
-    component: () =>
-      import("@/pages/Entity/Category/Form.vue"),
-    meta: {
-      breadcrumbs: ["dashboard", "categoryList", "categoryAdd"]
-    }
-  },
+    /* Category */
+    {
+        name: "categoryList",
+        path: "/categories",
+        component: () =>
+            import("@/pages/Entity/Category/List.vue"),
+        meta: {
+            title: "CategoryList",
+            breadcrumbs: ["dashboard", "categoryList"]
+        }
+    },
+    {
+        name: "categoryEdit",
+        path: "/categories/:id/edit",
+        component: () =>
+            import("@/pages/Entity/Category/Form.vue"),
+        meta: {
+            title: "CategoryEdit",
+            breadcrumbs: ["dashboard", "categoryList", "categoryEdit"]
+        }
+    },
+    {
+        name: "categoryAdd",
+        path: "/categories/add",
+        component: () =>
+            import("@/pages/Entity/Category/Form.vue"),
+        meta: {
+            title: "CategoryAdd",
+            breadcrumbs: ["dashboard", "categoryList", "categoryAdd"]
+        }
+    },
   /* News */
   {
     name: "newsList",
@@ -56,34 +59,37 @@ export default [
       breadcrumbs: ["dashboard", "newsList", "newsAdd"]
     }
   },
-  /* Tag */
-  {
-    name: "tagList",
-    path: "/tags",
-    component: () =>
-      import("@/pages/Entity/Tag/List.vue"),
-    meta: {
-      breadcrumbs: ["dashboard", "tagList"]
-    }
-  },
-  {
-    name: "tagEdit",
-    path: "/tags/:id/edit",
-    component: () =>
-      import("@/pages/Entity/Tag/Form.vue"),
-    meta: {
-      breadcrumbs: ["dashboard", "tagList", "tagEdit"]
-    }
-  },
-  {
-    name: "tagAdd",
-    path: "/tags/add",
-    component: () =>
-      import("@/pages/Entity/Tag/Form.vue"),
-    meta: {
-      breadcrumbs: ["dashboard", "tagList", "tagAdd"]
-    }
-  },
+    /* Tag */
+    {
+        name: "tagList",
+        path: "/tags",
+        component: () =>
+            import("@/pages/Entity/Tag/List.vue"),
+        meta: {
+            title: "TagList",
+            breadcrumbs: ["dashboard", "tagList"]
+        }
+    },
+    {
+        name: "tagEdit",
+        path: "/tags/:id/edit",
+        component: () =>
+            import("@/pages/Entity/Tag/Form.vue"),
+        meta: {
+            title: "TagEdit",
+            breadcrumbs: ["dashboard", "tagList", "tagEdit"]
+        }
+    },
+    {
+        name: "tagAdd",
+        path: "/tags/add",
+        component: () =>
+            import("@/pages/Entity/Tag/Form.vue"),
+        meta: {
+            title: "TagAdd",
+            breadcrumbs: ["dashboard", "tagList", "tagAdd"]
+        }
+    },
   /* City */
   {
     name: "cityList",
